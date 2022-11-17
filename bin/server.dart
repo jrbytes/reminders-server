28b35e3c81db98bcea7957d07cdc9aa67c87cc77
Jr Bytes <itamazor@gmail.com>
2022-11-16 23:22:21 -0300

    configured middleware log

--- bin/server.dart
+++ bin/server.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:reminders_server/app.dart';
+import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_hotreload/shelf_hotreload.dart';
 
@@ -9,5 +10,18 @@ Future<void> main(List<String> arguments) async {
 }
 
 Future<HttpServer> createServer() {
-  return io.serve(function, '0.0.0.0', 8080);
+  var pipeline = Pipeline().addMiddleware(log());
+
+  return io.serve(pipeline.addHandler(app), '0.0.0.0', 8080);
+}
+
+Middleware log() {
+  return (handler) {
+    return (request) async {
+      print('solicitado: ${request.url}');
+      var response = await handler(request);
+      print('[${response.statusCode}]');
+      return response;
+    };
+  };
 }
--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,33 @@
 import 'dart:async';
 
-import 'package:functions_framework/functions_framework.dart';
+import 'package:reminders_server/src/generated/prisma_client.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
-@CloudFunction()
-FutureOr<Response> function(Request request) async {
+final PrismaClient prisma = PrismaClient();
+
+FutureOr<Response> app(Request request) async {
   final router = Router();
 
-  router.get('/', (Request request) => Response.ok('Hello world!'));
+  router.get('/login', (Request request) async {
+    // final Project project = await prisma.project.create(
+    //   data: ProjectCreateInput(
+    //     name: 'Create reminders app',
+    //     emoji: ':tada:',
+    //     color: '#6B46C1',
+    //   ),
+    // );
+
+    // print(project.toJson());
+
+    return Response.ok('Hello, World!');
+  });
+
+  router.get('/projects', (Request request) async {
+    final project = await prisma.project.findMany();
+    print(project);
+    return Response.ok(project);
+  });
 
   return router(request);
 }
