6317d695f4a51c2d3c952b979f154fa76d59e163
Jr Bytes <itamazor@gmail.com>
2022-11-19 17:25:04 -0300

    updated create project usecase to remove id

--- lib/src/modules/projects/use_cases/create.dart
+++ lib/src/modules/projects/use_cases/create.dart
@@ -5,15 +5,17 @@ import 'package:shelf_modular/shelf_modular.dart';
 
 import 'package:reminders_server/src/generated/prisma_client.dart';
 
-final PrismaClient prisma = PrismaClient();
-
 FutureOr<Response> create(Injector injector, ModularArguments arguments) async {
   final database = injector.get<PrismaClient>();
+
+  final projectData = (arguments.data as Map).cast<String, dynamic>();
+  projectData.remove('id');
+
   final result = await database.project.create(
     data: ProjectCreateInput(
-      name: arguments.data['name'],
-      emoji: arguments.data['emoji'],
-      color: arguments.data['color'],
+      name: projectData['name'],
+      emoji: projectData['emoji'],
+      color: projectData['color'],
     ),
   );
   final response = result.toJson();
